53f3a435557341959f2fdc6a2e59ab36f7809865
jeftedamaceno <103365379+jeftedamaceno@users.noreply.github.com>
2023-12-04 21:05:18 -0300

    criando alteracao de valores por ano e mes step3

--- src/js/index.js
+++ src/js/index.js
@@ -26,12 +26,27 @@ radios.forEach((radio,)=>{
 const checkbox_time = document.querySelector('#switch');
 const months = document.querySelectorAll('.check-monthly');
 const years = document.querySelectorAll('.check-year.desativado');
+const year_check_paragrafo = document.getElementById('yearly')
+const monthly_check_paragrafo = document.getElementById('monthly')
+// variaveis do step 3 
+
+const months_step3 = document.querySelectorAll('.check-preco');
+const years_step3 = document.querySelectorAll('.check-preco-year.desativado');
+
 
 checkbox_time.addEventListener('click', ()=>{
     if(months[0].classList.contains('desativado')){
         alteraTextoCheckbox(months, years)
+        alteraTextoCheckbox(months_step3, years_step3)
+
+        monthly_check_paragrafo.classList.add('input-seleceinado')
+        year_check_paragrafo.classList.remove('input-seleceinado')
     }else{
         alteraTextoCheckbox(years, months)
+        alteraTextoCheckbox(years_step3, months_step3)
+        
+        monthly_check_paragrafo.classList.remove('input-seleceinado')
+        year_check_paragrafo.classList.add('input-seleceinado')
     }
 });
 
@@ -44,6 +59,7 @@ function desselecionarStep() {
     const stepSelecionado = document.querySelector(".botao_ativado");
     stepSelecionado.classList.remove("botao_ativado");
 }
+
 function desselecionarRadio() {
     if(document.querySelector(".radio-plano.radio-selecionado")){
         const radioSelecionado = document.querySelector(".radio-plano.radio-selecionado");
@@ -52,7 +68,6 @@ function desselecionarRadio() {
 }
 function alteraTextoCheckbox(lista_check1, lista_check2) {
     lista_check1.forEach((lista,indice)=>{ 
-        console.log('oi')
         lista.classList.remove('desativado')
         lista_check2[indice].classList.add('desativado')
     });
